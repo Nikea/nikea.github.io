344d9a47184b37866d4647dfb22931dbc14ec2ec
Eric Dill <edill@bnl.gov>
2014-06-12 14:10:51 -0400

    DOC: Fixed nsls2_github.sh

--- nsls2_github.sh
+++ nsls2_github.sh
@@ -1,19 +1,34 @@
 #! /bin/bash
-rm -rf vistools
-sitories to clone
+if [ ! -d ~/dev]
+    then
+        mkdir ~/dev
+fi
+cd ~/dev
+https="https://github.com/"
+ssh="git@github.com:"
+prefix=$https
+if ["$1" == "https"]
+    then
+        prefix=$https
+fi
+if ["$1" == "ssh"]
+    then
+        prefix=$ssh
+fi
+echo $prefix
+## respositories to clone
 declare -a repo_names=("NSLS2" "vistools" "KitchenSink" "NSLS-II.github.io")
 ## user names of developers
 declare -a user_names=("ericdill" "tacaswell" "sameera2004" "giltis" "jammcc" "stuwilkins" "CJ-Wright" "dchabot" "sbillinge")
 ## remote names for the devs branches
 declare -a alias=("dill" "caswell" "sameera" "iltis" "mcclintock" "wilkins" "wright" "chabot" "billinge")
-prefix="https://github.com/NSLS-II/"
 suffix=".git"
 ## loop through the repositories
 for repo in "${repo_names[@]}"
 do
     echo removing "$repo" repository
     rm -rf "$repo"
-    url="$prefix""$repo""$suffix"
+    url="$prefix""NSLS-II/""$repo""$suffix"
 ##    echo echoing repo_names: $url
     git clone $url
     cd "$repo"
@@ -21,7 +36,7 @@ do
     # loop through the user names
     for idx in "${!user_names[@]}"
     do
-        url=https://github.com/"${user_names[idx]}""/""$repo""$suffix"
+        url="$prefix""${user_names[idx]}""/""$repo""$suffix"
 ##        echo remote url: $url
         git remote add "${alias[idx]}" $url
 	done
